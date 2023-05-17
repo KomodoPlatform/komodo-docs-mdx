f5aa4b8b30d50e75c278e87d26e97601db355632
smk762 <smk762@iinet.net.au>
2023-05-17 15:28:37 +0800

    restore sidebar

--- src/data/sidebar.ts
+++ src/data/sidebar.ts
@@ -401,22 +401,8 @@ const atomicdexApi_20_Dev_PageNavigation: NavigationRouteType = {
           "href": "/atomicdex/api/v20-dev/lightning/"
         },
         {
-            "title": "Activation",
-            "titleLink": "/atomicdex/api/v20-dev/lightning/activation",
-            "links": [
-                {
-                    "title": "Lightning Initialization",
-                    "href": "/atomicdex/api/v20-dev/lightning/activation#initialize-lightning"
-                },
-                {
-                    "title": "Initialization Status",
-                    "href": "/atomicdex/api/v20-dev/lightning/activation#initialization-status"
-                },
-                {
-                    "title": "Cancel Initialization",
-                    "href": "/atomicdex/api/v20-dev/lightning/activation#cancel-initialization"
-                }
-            ]
+          "title": "Lightning Initialization",
+          "href": "/atomicdex/api/v20-dev/lightning/activation"
         },
         {
           "title": "Lightning Channels",
