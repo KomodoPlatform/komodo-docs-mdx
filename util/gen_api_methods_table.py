c9f80869a4d01c33525ebd3739c0cb6fbdf38d86
smk762 <smk762@iinet.net.au>
2023-05-19 03:10:23 +0800

    code cleanup

--- util/gen_api_methods_table.py
+++ util/gen_api_methods_table.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-
 import os
 import glob
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 root_path = os.path.dirname(script_path)
-
 
 def gen_api_methods_table():
     atomicdex_files = glob.glob(f'{root_path}/src/pages/atomicdex/**/index.mdx', recursive = True)
@@ -18,19 +16,18 @@ def gen_api_methods_table():
     for file in atomicdex_files:
         with open(file, 'r') as f:
             for line in f.readlines():
-                doc_url = file.replace(f'{root_path}/src/pages', '').replace('/index.mdx', '')
-                doc_split = doc_url.split('/')
+                doc_path = file.replace(f'{root_path}/src/pages', '').replace('/index.mdx', '')
+                doc_split = doc_path.split('/')
                 if len(doc_split) > 3:
                     section = doc_split[3]
                     if section in methods_dict:
                         if 'CodeGroup' in line and "label" in line:
-                            line = line.strip()
                             method = line.split('label="')[1].split('"')[0]
-                            link = f"[{method}]({doc_url}#{method})"
+                            link = f"[{method}]({doc_path}#{method})"
                             methods_dict[section].append({
                                 "link": link,
                                 "method": method,
-                                "doc_url": doc_url
+                                "doc_url": doc_path
                             })
                             methods_list.append(method)
     methods_list = sorted(list(set(methods_list)))
@@ -55,9 +52,6 @@ def gen_api_methods_table():
                 line = "| {:^108} | {:^108} | {:^108} |".format(legacy, v20, v20_dev)
                 f2.write(f"{line}\n")
 
-                
-
 
 if __name__ == '__main__':
     gen_api_methods_table()
-    pass
