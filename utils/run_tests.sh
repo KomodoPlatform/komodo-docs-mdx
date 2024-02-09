7c6aba404a7f7c58b8b426109910cb972e7a41a4
gaeacodes <gaeacodes@gmail.com>
2024-02-09 14:55:22 +0530

    check komodo and defi versions are latest or not

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -8,4 +8,5 @@ cd ../../
 node utils/js/get_file_author_data.js
 node utils/js/validate_update_internal_links_userpass.js
 node utils/js/file_presence_structure_checker.js
+node utils/js/changelog_versions.js
 node utils/js/h1_presence_checker.js
