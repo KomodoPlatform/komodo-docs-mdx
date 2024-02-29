f2f19be5c42800490429925eccc8dd54bcbcd63e
gcharang <21151592+gcharang@users.noreply.github.com>
2024-02-29 18:00:55 +0530

    update tests

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -5,7 +5,7 @@
 
 cd js && npm ci
 cd ../../
-node utils/js/get_file_author_data.js
+#node utils/js/get_file_author_data.js
 node utils/js/validate_update_internal_links_userpass.js
 node utils/js/file_presence_structure_checker.js
 node utils/js/h1_presence_checker.js
