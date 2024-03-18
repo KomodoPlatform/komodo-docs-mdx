daeeded47bb909620eb2c116d6ce89b6ce5daa40
gcharang <21151592+gcharang@users.noreply.github.com>
2024-03-18 20:16:00 +0530

    adds sitemap, constants

---
+++ utils/js/constants.js
@@ -0,0 +1,17 @@
+export const constants = {
+    "contentRepo": "https://github.com/KomodoPlatform/komodo-docs-mdx",
+    "domain": "https://komodoplatform.com",
+    "basePath": "/en/docs",
+    "pngLogo": "/assets/img/logo-dark.png",
+    "supportEmail": "support@komodoplatform.com",
+    "helloEmail": "hello@komodoplatform.com",
+    "socialLinks": {
+        "discord": "https://komodoplatform.com/discord",
+        "telegram": "https://t.me/KomodoPlatform_Official",
+        "linkedin": "https://mt.linkedin.com/company/komodoplatform",
+        "reddit": "https://www.reddit.com/r/komodoplatform/",
+        "github": "https://www.github.com/KomodoPlatform",
+        "twitter": "https://twitter.com/KomodoPlatform/",
+        "youtube": "https://www.youtube.com/channel/UCtp7YrrEnZbW97sgPldyvKg"
+    }
+}
