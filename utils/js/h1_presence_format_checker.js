3e23bf55e8dc7ad475fe1b782d13c27891be9851
gcharang <21151592+gcharang@users.noreply.github.com>
2024-07-22 16:19:32 +0530

    ignore checking files that satrt with . in testsd

--- utils/js/h1_presence_format_checker.js
+++ utils/js/h1_presence_format_checker.js
@@ -1,5 +1,5 @@
 //import { visit, EXIT } from 'unist-util-visit'
-import {EXIT, visitParents} from 'unist-util-visit-parents'
+import { EXIT, visitParents } from 'unist-util-visit-parents'
 
 import { promises as fs } from 'fs';
 import path from 'path'
@@ -12,23 +12,23 @@ import remarkMdx from "remark-mdx";
             const markdown = await fs.readFile(filePath, 'utf-8');
             //console.log(filePath)
             const file = await remark()
-            .use(remarkGfm)
-            .use(remarkMdx)
+                .use(remarkGfm)
+                .use(remarkMdx)
                 .use(() => (tree) => {
                     let documentContainsTitleHeading = false;
                     let numH1s = 0
-                    visitParents(tree, 'heading', (node, ancestors) => {                       
-                        let nodeHasLinks = node.children.some(child => child.type === "link") 
-                        if(nodeHasLinks){
-                            throw new Error(`${JSON.stringify(node,null,2)} 
+                    visitParents(tree, 'heading', (node, ancestors) => {
+                        let nodeHasLinks = node.children.some(child => child.type === "link")
+                        if (nodeHasLinks) {
+                            throw new Error(`${JSON.stringify(node, null, 2)} 
 in file:${filePath} has a link. Please remove it.`)
-                        }                                            
+                        }
                         if (node.depth === 1 && !ancestors.some((ancestor) => ancestor.name === "DevComment")) {
                             documentContainsTitleHeading = true;
                             numH1s = numH1s + 1
                             if (numH1s > 1) {
                                 throw new Error(`Document must contain only one <h1>: ${filePath}
-node: ${JSON.stringify(node,null,2)}`);
+node: ${JSON.stringify(node, null, 2)}`);
                             }
                             //EXIT; 
                         }
@@ -51,7 +51,7 @@ async function walkDir(dir, callback) {
         const stats = await fs.stat(filePath);
         if (stats.isDirectory()) {
             await walkDir(filePath, callback);
-        } else if (stats.isFile()) {
+        } else if (stats.isFile() && !filePath.startsWith(".")) {
             await callback(filePath);
         }
     }));
